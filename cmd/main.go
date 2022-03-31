f59081bfd70d239e8a736ddaf64248212ae26f17
zhaozeng <zhaozeng@haohaozhu.com>
2022-03-31 10:56:37 +0800

    config init

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"delay_mq_v2/conf"
+	"delay_mq_v2/http"
+	"delay_mq_v2/service"
 	"fmt"
+)
+
+var (
+	srv *service.Service
 )
 
 func main()  {
 	err := conf.Init()
-	fmt.Println(err)
-	fmt.Println("1")
+	if err != nil {
+		fmt.Printf("conf init error#%v", err)
+		return
+	}
+	srv = service.New(conf.Conf)
+	http.Start(conf.Conf, srv)
 }
