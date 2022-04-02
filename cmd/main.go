2a5c09def7503a1ec093d9d07905f2bec21bc304
zhaozeng <zhaozeng@haohaozhu.com>
2022-04-02 10:37:01 +0800

    安全退出

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
 	"delay_mq_v2/conf"
 	"delay_mq_v2/http"
 	"delay_mq_v2/service"
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 var (
@@ -12,11 +18,30 @@ var (
 )
 
 func main()  {
+	ctx, cancel := context.WithCancel(context.Background())
 	err := conf.Init()
 	if err != nil {
 		fmt.Printf("conf init error#%v", err)
 		return
 	}
 	srv = service.New(conf.Conf)
-	http.Start(conf.Conf, srv)
+	go srv.Run(ctx)
+	go http.Start(conf.Conf, srv)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	for {
+		s := <-c
+		log.Printf("delay mq service get a signal %s", s.String())
+		switch s {
+		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			cancel()
+			log.Printf("delay mq service exit")
+			time.Sleep(time.Second)
+			return
+		case syscall.SIGHUP:
+		default:
+			return
+		}
+	}
 }
